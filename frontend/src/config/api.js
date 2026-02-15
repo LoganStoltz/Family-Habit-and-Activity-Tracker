9a1909910da677de9f5729329fd6ab3fb145ff38
Logan Stoltz <102991130+LoganStoltz@users.noreply.github.com>
2026-02-14 21:59:57 -0800

    Update api.js

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,7 +1,7 @@
 // src/api.js
 export const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL ||
-  'http://ec2-18-116-66-13.us-east-2.compute.amazonaws.com:3000' // make sure port matches Puma
+  'http://18.116.66.13' // make sure port matches Puma
 
 export const apiRequest = async (endpoint, options = {}) => {
   const url = `${API_BASE_URL}${endpoint}`
